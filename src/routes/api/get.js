5532efe8cf0ea6afcd68ccc8d802b8bb2e2baa02
batunpc <batuhnpc@gmail.com>
2022-09-21 03:52:40 -0400

    preparation of lab3

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -3,7 +3,7 @@
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
+module.exports = (_, res) => {
   // TODO: this is just a placeholder to get something working...
   res.status(200).json({
     status: 'ok',
