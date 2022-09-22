0ff93b3b6d01222ad3bf8eb37eb76c79a6f7d49f
batunpc <batuhnpc@gmail.com>
2022-09-22 02:36:54 -0400

    ROUTE:/ refactor to use response.js

--- src/response.js
+++ src/response.js
@@ -1,13 +1,5 @@
 // src/response.js
 
-/**
- * A successful response looks like:
- *
- * {
- *   "status": "ok",
- *   ...
- * }
- */
 module.exports.createSuccessResponse = function (data) {
   return {
     status: 'ok',
@@ -16,18 +8,6 @@ module.exports.createSuccessResponse = function (data) {
     ...data,
   };
 };
-
-/**
- * An error response looks like:
- *
- * {
- *   "status": "error",
- *   "error": {
- *     "code": 400,
- *     "message": "invalid request, missing ...",
- *   }
- * }
- */
 module.exports.createErrorResponse = function (code, message) {
   return {
     status: 'error',
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 // src/routes/index.js
-
 const express = require('express');
 
 // version and author from package.json
-const { version, author } = require('../../package.json');
+const { version, author, homepage } = require('../../package.json');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
 const { authenticate } = require('../authorization/index');
+const { createSuccessResponse } = require('../response');
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  * Protect them all so you have to be authenticated in order to access.
@@ -21,13 +21,8 @@ router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
   // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // Use your own GitHub URL for this...
-    githubUrl: 'https://github.com/batunpc/fragments',
-    version,
-  });
+  res.status(200).json(createSuccessResponse({ version, author, homepage })); //response
+  console.log(homepage);
 });
 
 module.exports = router;
