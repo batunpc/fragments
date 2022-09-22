3ceea2dc7edf397f3883bd2891421fef8ece5522
batunpc <batuhnpc@gmail.com>
2022-09-22 03:04:50 -0400

    RF get.js for the route /v1/fragments w response.js functions

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,12 +1,10 @@
 // src/routes/api/get.js
+const { createSuccessResponse } = require('../../response');
 
 /**
  * Get a list of fragments for the current user
  */
 module.exports = (_, res) => {
   // TODO: this is just a placeholder to get something working...
-  res.status(200).json({
-    status: 'ok',
-    fragments: [],
-  });
+  res.status(200).json(createSuccessResponse({ fragments: [] }));
 };
