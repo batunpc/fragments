f21ae7a99328046948b2d03a472335fecece4391
batunpc <batuhnpc@gmail.com>
2022-10-04 02:39:29 -0400

    todo : getId.test.js

--- tests/unit/getId.test.js
+++ tests/unit/getId.test.js
@@ -13,9 +13,22 @@ describe('GET /v1/fragments/:id', () => {
   });
 
   test('should return a 404 for an invalid fragment id', async () => {
-    const response = await request(app)
-      .get('/v1/fragments/123')
+    const res = await request(app).get('/v1/fragments/123').auth('user1@email.com', 'password1');
+    expect(res.status).toBe(404);
+  });
+
+  test('should return a 200 and fragmen data for a valid fragment id', async () => {
+    const res = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('This is fragment');
+    const id = JSON.parse(res.text).fragment.id;
+    console.log(id);
+    const getRes = await request(app)
+      .get(`/v1/fragments/${id}`)
       .auth('user1@email.com', 'password1');
-    expect(response.status).toBe(404);
+    expect(getRes.status).toBe(200);
+    //TODO: convert This is fragment to buffer and compare
   });
 });
