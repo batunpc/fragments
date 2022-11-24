dd45af80df7d99f97b3e3ef1b2d3fdc156889ac1
batunpc <batuhnpc@gmail.com>
2022-11-24 00:54:40 -0500

    maintanance: updated tests

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -19,7 +19,7 @@ describe('GET /v1/fragments', () => {
   test('authenticated users can have empty array of fragments', async () => {
     const response = await validGetReq('/v1/fragments');
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({ status: 'ok', data: [] });
+    expect(response.body).toEqual({ status: 'ok', fragments: [] });
   });
 
   test('authenticated users get a fragments array of ids: user with fragments', async () => {
@@ -34,8 +34,26 @@ describe('GET /v1/fragments', () => {
     const response = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
 
     expect(response.status).toBe(200);
-    expect(response.body.data).toEqual(expect.arrayContaining([fragmentId]));
-    expect(response.body).toEqual({ status: 'ok', data: [fragmentId] });
+    expect(response.body.fragments).toEqual(expect.arrayContaining([fragmentId]));
+    expect(response.body).toEqual({ status: 'ok', fragments: [fragmentId] });
+  });
+
+  // test for expand=1
+  test('authenticated users get a fragments array of objects: user with fragments', async () => {
+    const req = await request(app)
+      .post('/v1/fragments')
+      .auth('user1@email.com', 'password1')
+      .set('Content-Type', 'text/plain')
+      .send('Chewbacca!');
+    const body = JSON.parse(req.text);
+
+    const fragmentId = body.fragment.id;
+    const response = await request(app)
+      .get('/v1/fragments?expand=1')
+      .auth('user1@email.com', 'password1')
+      .expect(200);
+
+    expect(response.body.fragments).toEqual(expect.arrayContaining([fragmentId]));
   });
 
   test('request fail', async () => {
