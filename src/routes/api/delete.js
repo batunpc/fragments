f8960afd3d57eb7364ddebd91c2d02db723efe49
batunpc <batuhnpc@gmail.com>
2022-11-27 18:59:07 -0500

    updated delete route

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -2,7 +2,7 @@ const { Fragment } = require('../../model/fragment');
 const { createSuccessResponse, createErrorResponse } = require('../../response');
 
 module.exports = async (req, res) => {
-  const id = req.params.id.split('.')[0];
+  const id = req.params.id;
   try {
     const fragment = await Fragment.byId(req.user, id);
     if (!fragment) return res.status(404).json(createErrorResponse(404, 'Id not found'));
