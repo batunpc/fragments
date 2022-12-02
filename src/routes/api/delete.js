61eb8ee560cb1a7057e2b03ef23b3753d6f9ba42
batunpc <batuhnpc@gmail.com>
2022-12-01 19:18:15 -0500

    update delete route with tests

--- src/routes/api/delete.js
+++ src/routes/api/delete.js
@@ -4,12 +4,10 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 module.exports = async (req, res) => {
   const id = req.params.id;
   try {
-    const fragment = await Fragment.byId(req.user, id);
-    if (!fragment) return res.status(404).json(createErrorResponse(404, 'Id not found'));
-
+    await Fragment.byId(req.user, id);
     await Fragment.delete(req.user, id);
     res.status(200).json(createSuccessResponse());
-  } catch (e) {
-    res.status(500).json(createErrorResponse(500, e.message));
+  } catch (err) {
+    res.status(404).json(createErrorResponse(404, err.message));
   }
 };
